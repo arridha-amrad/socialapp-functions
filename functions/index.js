3c45f026274393866d987fc04106ef997ff56e27
arridha-amrad <arridhaamrad@gmail.com>
2019-09-15 16:44:49 +0700

    backend complete

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,8 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const Auth = require('./middleware/Auth');
 const { getAllScreams, createNewScream, getScream, commentOnScream, likeScream, unlikeScream, deleteScream } = require('./handlers/Scream');
-const { Signup, Signin, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/User');
+const { Signup, Signin, uploadImage, addUserDetails, getAuthenticatedUser, getUserDetails, markNotificationsRead } = require('./handlers/User');
+const {db} = require('./utils/Admin');
 
 app.get('/scream', getAllScreams);
 app.post('/scream', Auth, createNewScream);
@@ -19,5 +20,55 @@ app.post('/login', Signin);
 app.post('/user/image', Auth, uploadImage);
 app.post('/user', Auth, addUserDetails);
 app.get('/user', Auth, getAuthenticatedUser);
+app.get('/user/:handle', getUserDetails);
+app.post('/notifications', Auth, markNotificationsRead)
 
 exports.api = functions.region('asia-northeast1').https.onRequest(app);
+
+exports.createNotificationOnLike = functions.region('asia-northeast1').firestore.document('likes/{id}')
+.onCreate((snapshot) => {
+  return db.doc(`/screams/${snapshot.data().screamId}`)
+  .get()
+  .then(doc => {
+    if(doc.exists) {
+      return db.doc(`/notification/${snapshot.id}`).set({
+        createdAt: new Date().toISOString(),
+        recipient: doc.data().userHandle,
+        sender: snapshot.data().userHandle,
+        type: 'like',
+        read: false,
+        screamId: doc.id
+      });
+    }
+  })
+  .catch(err => console.error(err))
+})
+
+exports.deleteNotificationOnUnlike = functions.region('asia-northeast1').firestore.document('likes/{id}')
+.onDelete(snapshot => {
+  return db.doc(`notification/${snapshot.id}`)
+    .delete()
+    .catch(err => {
+      console.error(err)
+      return;
+    });
+});
+
+exports.createNotificationOnComment = functions.region('asia-northeast1').firestore.document('comments/{id}')
+.onCreate((snapshot) => {
+  return db.doc(`/screams/${snapshot.data().screamId}`)
+  .get()
+  .then(doc => {
+    if(doc.exists) {
+      return db.doc(`/notification/${snapshot.id}`).set({
+        createdAt: new Date().toISOString(),
+        recipient: doc.data().userHandle,
+        sender: snapshot.data().userHandle,
+        type: 'comment',
+        read: false,
+        screamId: doc.id
+      });
+    }
+  })
+  .catch(err => console.error(err))
+})
